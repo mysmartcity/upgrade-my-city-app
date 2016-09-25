967c726fe47f5eb8000b78da6717c94fe09e0c35
Petru Isfan <petru.isfan@gmail.com>
2016-09-25 19:33:57 +0300

    Initial import

---
+++ app/pages/tabs/tabs.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+import { NavParams } from 'ionic-angular';
+
+import { AboutPage } from '../about/about';
+import { MapPage } from '../map/map';
+//import { ProjectPage } from '../project/project';
+import { SchedulePage } from '../schedule/schedule';
+import { SpeakerListPage } from '../speaker-list/speaker-list';
+
+
+@Component({
+  templateUrl: 'build/pages/tabs/tabs.html'
+})
+export class TabsPage {
+  // set the root pages for each tab
+  tab1Root: any = AboutPage;
+  tab2Root: any = SchedulePage;
+  tab3Root: any = SpeakerListPage;
+  tab4Root: any = SpeakerListPage;
+  mySelectedIndex: number;
+
+  constructor(navParams: NavParams) {
+    this.mySelectedIndex = navParams.data.tabIndex || 0;
+  }
+}
